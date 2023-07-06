4b1cdf5b6b27832d816f00a57384d803d7258e84
HamzaGulraiz <110945038+HamzaGulraiz@users.noreply.github.com>
2023-07-06 22:21:53 +0500

    fixed

---
+++ config.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'http://192.168.0.102:8000';
+// export const BASE_URL =
+//   'mongodb+srv://ambuserve:PsoaEj9TRUeECDas@ambuserveadmin.9dzzo6b.mongodb.net/?retryWrites=true&w=majority';
