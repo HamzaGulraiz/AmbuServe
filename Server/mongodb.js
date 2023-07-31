3f1ad92298a80edf2a396f035e8d55608d2e19b1
HamzaGulraiz <110945038+HamzaGulraiz@users.noreply.github.com>
2023-07-31 20:26:35 +0500

    server files added

---
+++ Server/mongodb.js
@@ -0,0 +1,11 @@
+const {MongoClient} = require('mongodb');
+const url = 'mongodb://0.0.0.0:27017';
+const databaseName = 'ambuserve';
+const client = new MongoClient(url);
+
+async function dbConnect() {
+  let result = await client.connect();
+  db = result.db(databaseName);
+  return db.collection('registeration');
+}
+module.exports = dbConnect;
